8fcb62b71841e466f35ad8e03d97da0781c3b8f6
KirrBrest <lukashchuk.kiryl@yandex.by>
2024-04-16 23:59:01 +0300

    feat: nazvanie_vashego_commita (Tue, Apr 16, 2024 11:59:01 PM)

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -111,8 +111,10 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((resource) =>
+    resource.map((el) => (el.status === 'fulfilled' ? el.value : null))
+  );
 }
 
 /**
@@ -133,8 +135,12 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce((acc, promise) => {
+    return acc.then((res) => {
+      return promise.then((el) => res + el.toString());
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
